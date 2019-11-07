4715927bcb3ddcee53502f56ad5bc9de1da19ed9
Adam Haile <adammhaile@gmail.com>
2019-11-06 19:21:15 -0500

    rework build process

--- build.sh
+++ build.sh
@@ -12,15 +12,14 @@ rm -rf "${BASE_CODE_DIR}/*"
 mkdir -p ${BASE_RELEASE_DIR}
 rm -rf "${BASE_RELEASE_DIR}/*"
 
-for f in ./configs/*.h;
+for d in ./configs/*/;
 do
-    echo "$f"
-    filename=$(basename -- "$f")
-    cfg_name="${filename%.*}"
+    echo "$d"
+    cfg_name=$(basename -- "$d")
     mkdir -p "${BASE_BUILD_DIR}/$cfg_name"
     mkdir -p "${BASE_CODE_DIR}/$cfg_name/grbl/"
     cp -r ./grbl/* "${BASE_CODE_DIR}/$cfg_name/grbl/"
-    cp -f ${f} "${BASE_CODE_DIR}/$cfg_name/grbl/config.h"
+    cp -rf ${d}/* "${BASE_CODE_DIR}/$cfg_name/grbl/"
 
     arduino --verify --board arduino:avr:nano:cpu=atmega328 --pref build.path="${BASE_BUILD_DIR}/$cfg_name" "${BASE_CODE_DIR}/$cfg_name/grbl/grbl.ino"
 
