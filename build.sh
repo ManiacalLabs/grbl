701ed03370493bc9d2e61d551596166e89392463
Adam Haile <adammhaile@gmail.com>
2019-11-06 23:28:33 -0500

    adding m3servo config new build script features

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-set -x
+# set -x
 set -e
 BASE_BUILD_DIR="./build/builds"
 BASE_CODE_DIR="./build/code"
@@ -12,8 +12,8 @@ rm -rf "${BASE_CODE_DIR}/*"
 mkdir -p ${BASE_RELEASE_DIR}
 rm -rf "${BASE_RELEASE_DIR}/*"
 
-for d in ./configs/*/;
-do
+build_config() {
+    d=$1
     echo "$d"
     cfg_name=$(basename -- "$d")
     mkdir -p "${BASE_BUILD_DIR}/$cfg_name"
@@ -21,8 +21,20 @@ do
     cp -r ./grbl/* "${BASE_CODE_DIR}/$cfg_name/grbl/"
     cp -rf ${d}/* "${BASE_CODE_DIR}/$cfg_name/grbl/"
 
-    arduino --verify --board arduino:avr:nano:cpu=atmega328 --pref build.path="${BASE_BUILD_DIR}/$cfg_name" "${BASE_CODE_DIR}/$cfg_name/grbl/grbl.ino"
-
+    arduino --verify --board arduino:avr:nano:cpu=atmega328 --pref build.path="${BASE_BUILD_DIR}/$cfg_name" --pref compiler.warning_level="none" "${BASE_CODE_DIR}/$cfg_name/grbl/grbl.ino" 2>&1 | awk '!/javax/'
+    
     cp "${BASE_BUILD_DIR}/$cfg_name/grbl.ino.hex" "${BASE_RELEASE_DIR}/$cfg_name.hex"
+}
+
+if [[ -z $1 ]]; then
+    for d in ./configs/*/;
+    do
+        build_config $d
+    done
+else
+    for d in "$@"
+do
+    build_config "./configs/$d"
 done
+fi
 
