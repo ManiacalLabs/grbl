536ec8714b466aa49c6093e8e0b2a319d76fe8c6
Adam Haile <adammhaile@gmail.com>
2020-01-05 12:34:10 -0500

    Now provide build sources

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # set -x
 set -e
-BASE_BUILD_DIR="./build/builds"
-BASE_CODE_DIR="./build/code"
-BASE_RELEASE_DIR="./build/release"
+BASE_BUILD_DIR="./dist/builds"
+BASE_CODE_DIR="./dist/code"
+BASE_RELEASE_DIR="./dist/release"
 mkdir -p ${BASE_BUILD_DIR}
 rm -rf "${BASE_BUILD_DIR}/*"
 
@@ -23,7 +23,10 @@ build_config() {
 
     arduino --verify --board arduino:avr:nano:cpu=atmega328 --pref build.path="${BASE_BUILD_DIR}/$cfg_name" --pref compiler.warning_level="none" "${BASE_CODE_DIR}/$cfg_name/grbl/grbl.ino" 2>&1 | awk '!/javax/'
     
-    cp "${BASE_BUILD_DIR}/$cfg_name/grbl.ino.hex" "${BASE_RELEASE_DIR}/$cfg_name.hex"
+    mkdir -p "${BASE_RELEASE_DIR}/$cfg_name/"
+    cp "${BASE_BUILD_DIR}/$cfg_name/grbl.ino.hex" "${BASE_RELEASE_DIR}/$cfg_name/$cfg_name.hex"
+    ls -al "${BASE_CODE_DIR}/$cfg_name/grbl/"
+    cp -f "${BASE_CODE_DIR}/$cfg_name/grbl/README.md" "${BASE_RELEASE_DIR}/$cfg_name/"
 }
 
 if [[ -z $1 ]]; then
@@ -37,4 +40,8 @@ do
     build_config "./configs/$d"
 done
 fi
+
+rm -rf ${BASE_BUILD_DIR}
+cp ./BUILDS_README.md ./dist/README.md
+rm -f ./build/.gitignore
 
